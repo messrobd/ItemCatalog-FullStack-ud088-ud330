d250b8e0305e79883cf52e9d5cc0d28f81057ce2
Rob Messenger <robmessenger@yahoo.com>
2018-12-13 14:04:58 +0100

    Add selection menus for type and milk
    Add selection menus for type and milk to the new and edit cheese
    forms. required standardisation on type instead of variety, and
    introduction of milk class
    
    The milk property of the cheese being edited is pre-selected in the
    edit form

--- models.py
+++ models.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
 
-class Variety():
+class Type():
     def __init__(self, id, name, description):
         self.id = id
         self.name = name
         self.description = description
 
+class Milk():
+    def __init__(self, name):
+        self.name = name
+
 class Cheese():
-    def __init__(self, id, name):
+    def __init__(self, id, name, type, milk):
         self.id = id
         self.name = name
+        self.type = type
+        self.milk = milk
--- views.py
+++ views.py
@@ -4,21 +4,27 @@ from flask import \
     render_template, \
     url_for
 from models import \
-    Variety, \
+    Type, \
+    Milk, \
     Cheese
 
 app = Flask(__name__)
 
-varieties = [
-    Variety(0, 'Blue cheese', 'moldy and good'),
-    Variety(1, 'Brown cheese', 'fudgy and weird')]
+types = [
+    Type(0, 'Blue cheese', 'moldy and good'),
+    Type(1, 'Brown cheese', 'fudgy and weird')]
+milks = [
+    Milk('cow'),
+    Milk('goat'),
+    Milk('sheep'),
+    Milk('buffalo')]
 cheeses = [
-    Cheese(0, 'stilton'),
-    Cheese(1, 'gorgonzola'),
-    Cheese(2, 'roquefort'),
-    Cheese(3, 'selbu'),
-    Cheese(4, 'shropshire')]
-cheeses += [Cheese(5, 'gorgonzola dolce')]
+    Cheese(0, 'stilton', 'Blue cheese', 'cow'),
+    Cheese(1, 'gorgonzola', 'Blue cheese', 'cow'),
+    Cheese(2, 'roquefort', 'Blue cheese', 'cow'),
+    Cheese(3, 'selbu', 'Blue cheese', 'cow'),
+    Cheese(4, 'shropshire', 'Blue cheese', 'cow')]
+cheeses += [Cheese(5, 'Gudbrandsdalsost', 'Brown cheese', 'goat')]
 
 # page view handlers
 @app.route('/')
@@ -27,16 +33,16 @@ def get_index():
     more_cheese = len(cheeses) > 5
     preview_list = 5 if more_cheese else len(cheeses)
     return render_template('catalog.html', \
-        varieties=varieties, \
+        types=types, \
         cheeses=cheeses, \
         preview_list=preview_list, \
         more_cheese=more_cheese)
 
-@app.route('/catalog/variety/<int:variety_id>')
-def get_cheeses(variety_id):
+@app.route('/catalog/type/<int:type_id>')
+def get_cheeses(type_id):
     return render_template('cheeses.html', \
-    variety=varieties[variety_id], \
-    cheeses=cheeses)
+        type=types[type_id], \
+        cheeses=cheeses)
 
 @app.route('/catalog/cheese/<int:cheese_id>')
 def get_cheese(cheese_id):
@@ -44,11 +50,16 @@ def get_cheese(cheese_id):
 
 @app.route('/catalog/cheese/new')
 def new_cheese():
-    return render_template('new_cheese.html')
+    return render_template('new_cheese.html', \
+        types=types, \
+        milks=milks)
 
 @app.route('/catalog/cheese/<int:cheese_id>/edit')
 def edit_cheese(cheese_id):
-    return render_template('edit_cheese.html', cheese=cheeses[cheese_id])
+    return render_template('edit_cheese.html', \
+        cheese=cheeses[cheese_id], \
+        types=types, \
+        milks=milks)
 
 @app.route('/catalog/cheese/<int:cheese_id>/delete')
 def delete_cheese(cheese_id):
