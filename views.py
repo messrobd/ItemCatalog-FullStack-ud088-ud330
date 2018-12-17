370785d67d077d6b069436e964e801b2ae2f1747
Rob Messenger <robmessenger@yahoo.com>
2018-12-17 19:58:58 +0100

    Render views using DB operations
    Render views using database operations
    
    Database operations are decorated to ensure a single session scope for
    each operation

--- views.py
+++ views.py
@@ -16,43 +16,70 @@ engine = create_engine('sqlite:///cheese.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 
-# session = DBSession()
+# db operations
+def db_operation(operation):
+    def decorator(*args, **kwargs):
+        session = DBSession()
+        output = operation(session, *args, **kwargs)
+        session.close()
+        return output
+    return decorator
+
+@db_operation
+def get_items(session, kind):
+    return session.query(kind).all()
+
+@db_operation
+def get_filtered_items(session, kind, **filter_args):
+    return session.query(kind).filter_by(**filter_args)
+
+@db_operation
+def get_item(session, kind, **filter_args):
+    return session.query(kind).filter_by(**filter_args).one()
 
 # page view handlers
 @app.route('/')
 @app.route('/catalog')
 def get_index():
-    return render_template('catalog.html', \
-        types=types, \
-        cheeses=cheeses)
+    types = get_items(Type)
+    return render_template('catalog.html', types=types)
 
 @app.route('/catalog/type/<int:type_id>')
 def get_cheeses(type_id):
-    cheeses_of_type = filter(lambda c: c.type == type_id, cheeses)
+    type = get_item(Type, id=type_id)
+    cheeses_of_type = get_filtered_items(Cheese, type_id=type_id)
+    print('query executed')
     return render_template('cheeses.html', \
-        type=types[type_id], \
+        type=type, \
         cheeses=cheeses_of_type)
 
 @app.route('/catalog/cheese/<int:cheese_id>')
 def get_cheese(cheese_id):
-    return render_template('cheese.html', cheese=cheeses[cheese_id])
+    cheese = get_item(Cheese, id=cheese_id)
+    return render_template('cheese.html', cheese=cheese)
 
 @app.route('/catalog/cheese/new')
 def new_cheese():
+    types = get_items(Type)
+    milks = get_items(Milk)
     return render_template('new_cheese.html', \
         types=types, \
         milks=milks)
 
 @app.route('/catalog/cheese/<int:cheese_id>/edit')
 def edit_cheese(cheese_id):
+    cheese = get_item(Cheese, id=cheese_id)
+    types = get_items(Type)
+    milks = get_items(Milk)
     return render_template('edit_cheese.html', \
-        cheese=cheeses[cheese_id], \
+        cheese=cheese, \
         types=types, \
         milks=milks)
 
 @app.route('/catalog/cheese/<int:cheese_id>/delete')
 def delete_cheese(cheese_id):
-    return render_template('delete_cheese.html', cheese=cheeses[cheese_id])
+    cheese = get_item(Cheese, id=cheese_id)
+    return render_template('delete_cheese.html', cheese=cheese)
 
 @app.route('/login')
 def login():
